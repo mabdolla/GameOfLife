f70d43f81a65b5630df0d26a5a8135cd91989b57
LAPTOP-LE2T4C6G\Mohammad Abdolla <mohammad.abdolla95@gmail.com>
2017-02-14 00:02:22 +0100

    Sett 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,51 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
+
+public class Controller implements Initializable {
+
+    @FXML private Canvas canvas;
+    @FXML private Slider celleSlider;
+    GraphicsContext gc;
+
+    private byte [][] board = {
+            {1,0,0,1},
+            {0,1,1,0},
+            {0,1,1,0},
+            {1,0,0,1}
+    };
+
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources ){
+        draw();
+    }
+
+    public void lagSpillebrett () {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                double celleSTR = celleSlider.getValue();
+                if (board[i][j] == 0) {
+                    gc.setFill(Color.INDIANRED);
+                    gc.fillRect(50+10*i, 5+20*j, celleSTR,celleSTR);
+                } else if (board[i][j] == 1) {
+                    gc.setFill(Color.BLUE);
+                    gc.fillRect(50+10*i, 5+20*j, celleSTR, celleSTR);
+                }
+            }
+        }
+    }
+
+    public void draw() {
+        gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.DARKGREY);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        lagSpillebrett();
+    }
+
+
 }
