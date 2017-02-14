a58662b89a0f571130773a8b25b4b5574bc9a2fb
Fredrik-PC\Fredrik <s309856@stud.hioa.no>
2017-02-14 08:25:15 +0100

    endret litt meny

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Game Of Life");
-        primaryStage.setScene(new Scene(root, 650, 500));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
