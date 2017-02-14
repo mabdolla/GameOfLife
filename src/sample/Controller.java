b6334456695d3389dd30f41a66ed640bd943eb0d
LAPTOP-LE2T4C6G\Mohammad Abdolla <mohammad.abdolla95@gmail.com>
2017-02-14 15:50:50 +0100

    Sett 2
    (cherry picked from commit ae36199)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -17,7 +17,7 @@ public class Controller implements Initializable {
     GraphicsContext gc;
     @FXML private HBox CanvasHbox;
 
-    private byte [][] board {
+    private byte [][] board = {
             {1,0,0,1,0,0,0},
             {0,1,1,0,0,0,0},
             {0,1,1,0,0,0,0},
@@ -28,9 +28,9 @@ public class Controller implements Initializable {
             {0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0}
             {0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0}
+            {0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0},
     };
 
     public void initialize(java.net.URL location, java.util.ResourceBundle resources ){
